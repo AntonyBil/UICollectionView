660ecd6901b5a48d62ba2154eb164efb57ce4a48
AntonyBil <beletskiy.ant@gmail.com>
2023-04-23 20:44:23 +0300

    Initial commit

---
+++ 11_CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  11_CollectionView
+//
+//  Created by apple on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
